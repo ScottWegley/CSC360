081be8a23d515a1adbf51588107e2f2a04986da5
Scott Wegley <scbrwegley@protonmail.ch>
2023-08-28 08:58:22 -0700

    math operations

--- Week1/BinaryOperations.java
+++ Week1/BinaryOperations.java
@@ -5,4 +5,19 @@ import java.util.Scanner;
 
 public class BinaryOperations {
 
+    private static int sum(int a, int b) {
+        return a + b;
+    }
+
+    private static int product(int a, int b) {
+        return a * b;
+    }
+
+    private static int difference(int a, int b) {
+        return a - b;
+    }
+
+    private static int quotient(int a, int b) {
+        return a / b;
+    }
 }
