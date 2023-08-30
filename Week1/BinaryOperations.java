d5978ae33227623523cbc6ef808f7fef660d2924
Scott Wegley <scbrwegley@protonmail.ch>
2023-08-30 09:00:19 -0700

    added bit operations

--- Week1/BinaryOperations.java
+++ Week1/BinaryOperations.java
@@ -17,21 +17,52 @@ public class BinaryOperations {
             String operator = scanner.nextLine();
             switch (operator) {
                 case "+":
-                    System.out.println(int2binary(operandA) + " " + operator + " " + int2binary(operandB) + " = " + int2binary(sum(operandA, operandB)));
+                    System.out.println(int2binary(operandA,8) + " " + operator + " " + int2binary(operandB,8) + " = "
+                            + int2binary(sum(operandA, operandB),8));
                     break;
                 case "-":
                     System.out.println(
-                            int2binary(operandA) + " " + operator + " " + int2binary(operandB) + " = " + int2binary(difference(operandA, operandB)));
+                            int2binary(operandA,8) + " " + operator + " " + int2binary(operandB,8) + " = "
+                                    + int2binary(difference(operandA, operandB),8));
                     break;
                 case "*":
                     System.out
-                            .println(int2binary(operandA) + " " + operator + " " + int2binary(operandB) + " = " + int2binary(product(operandA, operandB)));
+                            .println(int2binary(operandA,8) + " " + operator + " " + int2binary(operandB,8) + " = "
+                                    + int2binary(product(operandA, operandB),8));
                     break;
                 case "/":
                     System.out
-                            .println(int2binary(operandA) + " " + operator + " " + int2binary(operandB) + " = " + int2binary(quotient(operandA, operandB)));
+                            .println(int2binary(operandA,8) + " " + operator + " " + int2binary(operandB,8) + " = "
+                                    + int2binary(quotient(operandA, operandB),8));
                     break;
-
+                case "<<":
+                    System.out.println(int2binary(operandA,8) + " " + operator + " " + int2binary(operandB,8) + " = "
+                            + int2binary(lShift(operandA, operandB),8));
+                    break;
+                case ">>":
+                    System.out.println(int2binary(operandA,8) + " " + operator + " " + int2binary(operandB,8) + " = "
+                            + int2binary(signRShift(operandA, operandB),8));
+                    break;
+                case ">>>":
+                    System.out.println(int2binary(operandA,8) + " " + operator + " " + int2binary(operandB,8) + " = "
+                            + int2binary(unsignRShift(operandA, operandB),8));
+                    break;
+                case "&":
+                    System.out.println(int2binary(operandA,8) + " " + operator + " " + int2binary(operandB,8) + " = "
+                            + int2binary(and(operandA, operandB),8));
+                    break;
+                case "|":
+                    System.out.println(int2binary(operandA,8) + " " + operator + " " + int2binary(operandB,8) + " = "
+                            + int2binary(inclOr(operandA, operandB),8));
+                    break;
+                case "^":
+                    System.out.println(int2binary(operandA,8) + " " + operator + " " + int2binary(operandB,8) + " = "
+                            + int2binary(exclOr(operandA, operandB),8));
+                    break;
+                case "~":
+                    System.out.println(operator + "" + int2binary(operandA,8) + " = "
+                            + int2binary(complement(operandA),8));
+                            break;
                 default:
                     throw new IllegalArgumentException("Your operator was invalid");
             }
@@ -60,39 +91,39 @@ public class BinaryOperations {
         return a / b;
     }
 
-    private static int and(int a, int b){
+    private static int and(int a, int b) {
         return a & b;
     }
 
-    private static int inclOr(int a, int b){
+    private static int inclOr(int a, int b) {
         return a | b;
     }
 
-    private static int exclOr(int a, int b){
+    private static int exclOr(int a, int b) {
         return a ^ b;
     }
 
-    private static int lShift(int a, int b){
+    private static int lShift(int a, int b) {
         return a << b;
     }
 
-    private static int signRShift(int a, int b){
+    private static int signRShift(int a, int b) {
         return a >> b;
     }
 
-    private static int unsignRShift(int a, int b){
+    private static int unsignRShift(int a, int b) {
         return a >>> b;
     }
 
-    private static int complement(int a){
+    private static int complement(int a) {
         return ~a;
     }
-    
-    private static String int2binary(int a){
+
+    private static String int2binary(int a, int bits) {
         int q = a;
         int r = 0;
         String result = "";
-        for(int i = 0; i < 32; i++){
+        for (int i = 0; i < bits; i++) {
             r = q & 0x01;
             result = r + result;
             q = q >>> 1;
