ceb47c1d6d833869ea4be1825e9da2a74164a569
Scott Wegley <scbrwegley@protonmail.ch>
2023-09-01 08:56:17 -0700

    left 4 and right 4 bits functions

--- BitOperations/BitOperations.java
+++ BitOperations/BitOperations.java
@@ -1,6 +1,13 @@
 package BitOperations;
 
 public class BitOperations {
+    public static byte left(byte _b) {
+        return (byte) ((_b >>> 4) & 0b00001111);
+    }
+
+    public static byte right(byte _b) {
+        return (byte) (_b & 0b00001111);
+    }
     public static String int2binary(int a, int bits) {
         int q = a;
         int r = 0;
