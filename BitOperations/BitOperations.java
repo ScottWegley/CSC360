ebb522df2859491a5b9475802ece6b257ade52f4
Scott Wegley <scbrwegley@protonmail.ch>
2023-09-01 08:58:13 -0700

    lrswap

--- BitOperations/BitOperations.java
+++ BitOperations/BitOperations.java
@@ -13,6 +13,12 @@ public class BitOperations {
         return (byte) ((_b >> 2) & 0b00111111);
     }
 
+    public static short lrswap(short _b) {
+        short l = (short) ((_b >>> 8) & 0b0000000011111111);
+        short r = (short) (_b << 8);
+        return (short) ((0b1111111111111111 & r) | l);
+    }
+
     public static String int2binary(int a, int bits) {
         int q = a;
         int r = 0;
