51337e6e8abfb41ca8ceda3ce6d18fa4be5871db
Scott Wegley <scbrwegley@protonmail.ch>
2023-09-08 08:56:31 -0700

    created working expander function

--- BitOperations2/BitOperations2.java
+++ BitOperations2/BitOperations2.java
@@ -1,12 +1,23 @@
 package BitOperations2;
 
+import Libraries.BitOperator;
+
 public class BitOperations2 {
     public static byte keyextractor(short _key, int _pos) {
         return 0;
     }
 
     public static byte expander(byte _byte) {
-        return 0;
+        byte outB = (byte) (0b00110000 & _byte);
+        outB = (byte) (outB << 2);
+        outB = (byte) (outB | (_byte & 0b00000011));
+        byte temp = (byte) (((0b00000100 & _byte) << 1) | ((0b00001000 & _byte) >>> 1));
+        outB = (byte) ((outB | temp) | (temp << 2));
+        return outB;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(BitOperator.int2binary(expander((byte) 0b11011001), 8));
     }
 
 }
