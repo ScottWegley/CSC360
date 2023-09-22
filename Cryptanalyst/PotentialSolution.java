8382bd7894ef7e865321d236313bb3bad4e5c207
scottwegley <scottwegley@gmail.com>
2023-09-22 16:04:02 -0700

    accuracy calculation function, empty to function to populate dictionary

--- Cryptanalyst/PotentialSolution.java
+++ Cryptanalyst/PotentialSolution.java
@@ -3,10 +3,31 @@ class PotentialSolution {
     private String text;
     private double accuracy;
 
+    private static ArrayList<String> dictionary = null;
     public PotentialSolution(String _s) {
         text = _s.toUpperCase().trim();
     }
+    private void calculateAccuracy() {
+        if (dictionary == null) {
+            initializeDictionary();
+        }
+        String[] toCheck = getText().split(" ");
+        int totalWords = toCheck.length;
+        int realWords = 0;
+        for (String s : toCheck) {
+            if (dictionary.contains(s)) {
+                realWords++;
+            }
+        }
+        accuracy = (double) realWords / (double) totalWords;
+    }
 
+    private static void initializeDictionary() {
+    }
+
+    public double getAccuracy() {
+        return accuracy;
+    }
     public String getText() {
         return text;
     }
