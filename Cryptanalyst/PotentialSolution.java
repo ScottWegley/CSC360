d0854857aa69ed37006115c7fe46090d65812a9f
scottwegley <scottwegley@gmail.com>
2023-09-22 16:04:59 -0700

    documentation

--- Cryptanalyst/PotentialSolution.java
+++ Cryptanalyst/PotentialSolution.java
@@ -1,16 +1,43 @@
+package Cryptanalyst;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
+
+/**
+ * A class to store the attempted decryption of a text and what percentage of
+ * the words are real words.
+ */
 class PotentialSolution {
 
+    /** The potentially decrypted text. */
     private String text;
+    /**
+     * The accuracy of the decrypted text. (The percentage of words present in the
+     * {@link #dictionary}.)
+     */
     private double accuracy;
 
+    /**
+     * List of the ~10000 most common english words.
+     */
     private static ArrayList<String> dictionary = null;
+
+    /**
+     * Creates a {@link PotentialSolution} from the specified string.
+     * 
+     * @param _s The text of the potential solution.
+     */
     public PotentialSolution(String _s) {
         text = _s.toUpperCase().trim();
         calculateAccuracy();
     }
+
+    /**
+     * Checks the words in {@link #text} against the ~10000 most
+     * common english words to provide a percentage accuracy. If the dictionary has
+     * not been initialized, this initializes it w/ {@link #initializeDictionary()}.
+     */
     private void calculateAccuracy() {
         if (dictionary == null) {
             initializeDictionary();
@@ -26,6 +53,10 @@ class PotentialSolution {
         accuracy = (double) realWords / (double) totalWords;
     }
 
+    /**
+     * Reads the ~10000 most common english words from a file and loads them
+     * into {@link #dictionary} which we check against.
+     */
     private static void initializeDictionary() {
         Scanner scanner = null;
         try {
@@ -40,9 +71,16 @@ class PotentialSolution {
         scanner.close();
     }
 
+    /**
+     * @return {@link #accuracy}
+     */
     public double getAccuracy() {
         return accuracy;
     }
+
+    /**
+     * @return {@link #text}
+     */
     public String getText() {
         return text;
     }
