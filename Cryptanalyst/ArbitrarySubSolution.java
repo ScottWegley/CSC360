e4ef9d1fed2b93edc6f391c3fa387ab78875fa14
scottwegley <scottwegley@gmail.com>
2023-09-23 03:48:02 -0700

    modified to be an array list

--- Cryptanalyst/ArbitrarySubSolution.java
+++ Cryptanalyst/ArbitrarySubSolution.java
@@ -1,12 +1,18 @@
 package Cryptanalyst;
 
+import java.util.ArrayList;
+
 public class ArbitrarySubSolution extends PotentialSolution{
 
-    private char[] alphabet;
+    private ArrayList<Character> alphabet;
 
-    public ArbitrarySubSolution(String _s, char[] _a) {
+    public ArbitrarySubSolution(String _s, ArrayList<Character> _a) {
         super(_s);
         alphabet = _a;
+    }
+
+    public ArrayList<Character> getAlphabet(){
+        return alphabet;
     }
     
 }
