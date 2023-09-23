390834d8c6d17d113f1bef4d8318b52071f57b50
scottwegley <scottwegley@gmail.com>
2023-09-23 01:01:30 -0700

    created a class to hold solutions for any substitution cipher

---
+++ Cryptanalyst/ArbitrarySubSolution.java
@@ -0,0 +1,12 @@
+package Cryptanalyst;
+
+public class ArbitrarySubSolution extends PotentialSolution{
+
+    private char[] alphabet;
+
+    public ArbitrarySubSolution(String _s, char[] _a) {
+        super(_s);
+        alphabet = _a;
+    }
+    
+}
