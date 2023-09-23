b396b16d0db5bda7a76995cac16d3fb71d92ae79
scottwegley <scottwegley@gmail.com>
2023-09-23 03:49:44 -0700

    useful static members for consts and global storage

--- Cryptanalyst/FrequencyAnalyst.java
+++ Cryptanalyst/FrequencyAnalyst.java
@@ -1,4 +1,10 @@
 package Cryptanalyst;
 public class FrequencyAnalyst {
 
+    static BigInteger TWENTY_SIX_FACTORIAL = new BigInteger("403291461126605635584000000");
+    public static final String QUOTE_TWO = "B SMVE M HPEMJ RSMR LKE HMY RSBQ KMRBLK WBGG PBQE UN GBVE LUR RSE RPUE JEMKBKC LI BRQ TPEEH WE SLGH RSEQE RPURSQ RL AE QEGI EVBHEKR RSMR MGG JEK MPE TPEMREH EOUMG";
+    public static final char[] BASE_ALPHABET = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+            'O',
+            'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+    public static ArrayList<ArrayList<Character>> alphabets = new ArrayList<>();
 }
