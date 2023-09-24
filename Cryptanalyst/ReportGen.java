4213e0bb8870aa9f52f0b166d2fae85dddf662d6
scottwegley <scottwegley@gmail.com>
2023-09-24 12:34:14 -0700

    stable report generator

--- Cryptanalyst/ReportGen.java
+++ Cryptanalyst/ReportGen.java
@@ -3,6 +3,7 @@ package Cryptanalyst;
 import java.io.File;
 import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -27,53 +28,56 @@ public class ReportGen {
         }
         freqScan.close();
         // LOAD FREQUENCY TABLE
-        File file = new File("C:\\Code\\CSC360\\Cryptanalyst\\dictionary.txt");
-        Scanner scanner = new Scanner(file);
-        File out = new File("C:\\Code\\CSC360\\Cryptanalyst\\outputs\\CUSTOM.txt");
-        FileWriter writer = new FileWriter(out);
-        ArrayList<Character> temp = new ArrayList<>();
-        String chars = "SW";
-        int[] occur = { 1, 1 };
-        if (occur.length != chars.length()) {
-            throw new Exception();
-        }
+        HashMap<Integer,Integer> dupeMap = new HashMap<>();
+        String chars = "BLQQWX";
+        ArrayList<String> reports = new ArrayList<>();
+        Scanner scanner = null;
+        for (int CHAR_TO_REPORT = 0; CHAR_TO_REPORT < chars.length(); CHAR_TO_REPORT++) {
+            File file = new File("C:\\Code\\CSC360\\Cryptanalyst\\dictionary.txt");
+            scanner = new Scanner(file);
+            ArrayList<Character> temp = new ArrayList<>();
+            while (scanner.hasNextLine()) {
+                String in = scanner.nextLine();
+                if (temp.size() == 26) {
+                    break;
+                }
+                // Only check the word if it's the same length as the word we're using as a mask
+                // (chars).
+                if (in.length() == chars.length()) {
+                    boolean skip = false;
+                    for (int i = 0; i < in.length() && !skip; i++) {
+                        if (currFreq.get(chars.charAt(i)).indexOf(in.toUpperCase().charAt(i)) == -1) {
+                            skip = true; // For every character in our mask, make sure the corresponding character in
+                                         // this word is in our frequency list.
+                        }
 
-        while (scanner.hasNextLine()) {
-            String in = scanner.nextLine();
-            if (temp.size() == 26) {
-                break;
-            }
-            // Only check the word if it's the same length as the word we're using as a mask
-            // (chars).
-            if (in.length() == chars.length()) {
-                boolean skip = false;
-                for (int i = 0; i < in.length() && !skip; i++) {
-                    if (currFreq.get(chars.charAt(i)).indexOf(in.toUpperCase().charAt(i)) == -1) {
-                        skip = true; // For every character in our mask, make sure the corresponding character in
-                                     // this word is in our frequency list.
+                        final int _i = i;
+
+                        if (in.chars().filter(c -> c == in.charAt(_i)).count() != chars.chars()
+                                .filter(c -> c == chars.charAt(_i)).count()) {
+                            skip = true;
+                        }
                     }
-
-                    final int _i = i;
-
-                    if (in.chars().filter(c -> c == in.charAt(_i)).count() > occur[i]) {
-                        skip = true;
+                    if (skip) {
+                        continue;
+                    }
+                    if (!temp.contains(in.charAt(CHAR_TO_REPORT))) {
+                        temp.add(in.charAt(CHAR_TO_REPORT));
+                        System.out.println(in);
                     }
                 }
-                if (skip) {
-                    continue;
-                }
-                int i = 1;
-                if (!temp.contains(in.charAt(i))) {
-                    temp.add(in.charAt(i));
-                    System.out.println(in);
-                }
-            }
 
-        }
-        for (Character t : temp) {
-            writer.write(t + "\n");
+            }
+            Collections.sort(temp);
+            String report = chars.charAt(CHAR_TO_REPORT) + ": ";
+            for (Character t : temp) {
+                report = report + Character.toUpperCase(t);
+            }
+            reports.add(report);
         }
         scanner.close();
-        writer.close();
+        for (String s : reports) {
+            System.out.println(s);
+        }
     }
 }
