1bde2085691b884ab7180d08e053faa8635cd8c0
scottwegley <scottwegley@gmail.com>
2023-09-23 18:06:28 -0700

    added quote three to start brute forcing

--- Cryptanalyst/FrequencyAnalyst.java
+++ Cryptanalyst/FrequencyAnalyst.java
@@ -12,6 +12,7 @@ public class FrequencyAnalyst {
     static HashMap<Character, ArrayList<Character>> currFreq = null;
     static BigInteger TWENTY_SIX_FACTORIAL = new BigInteger("403291461126605635584000000");
     public static final String QUOTE_TWO = "B SMVE M HPEMJ RSMR LKE HMY RSBQ KMRBLK WBGG PBQE UN GBVE LUR RSE RPUE JEMKBKC LI BRQ TPEEH WE SLGH RSEQE RPURSQ RL AE QEGI EVBHEKR RSMR MGG JEK MPE TPEMREH EOUMG";
+    public static final String QUOTE_THREE = "KMS FW IO BLQQWX KILYJZKMF KFA MWV XUKV OWHY ZWHMVYO ZKM SW BWY OWH KFA XUKV OWH ZKM SW BWY OWHY ZWHMVYO";
     public static final char[] BASE_ALPHABET = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
             'O',
             'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
