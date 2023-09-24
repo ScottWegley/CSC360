6def0b625b48a1b5f4d745cf18484164ce7292b4
scottwegley <scottwegley@gmail.com>
2023-09-24 02:04:12 -0700

    modified report generator to make life easier for tomorrow

--- Cryptanalyst/ReportGen.java
+++ Cryptanalyst/ReportGen.java
@@ -3,54 +3,66 @@ package Cryptanalyst;
 import java.io.File;
 import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class ReportGen {
 
     // KILYJZKMF
     public static void main(String[] args) throws Exception {
+        // LOAD FREQUENCY TABLE
+        File freqTxt = new File("C:\\Code\\CSC360\\Cryptanalyst\\FreqTestQ3.txt");
+        Scanner freqScan = new Scanner(freqTxt);
+        HashMap<Character, String> currFreq = new HashMap<>();
+        while (freqScan.hasNextLine()) {
+            String in = freqScan.nextLine();
+            char header = in.charAt(1);
+            char[] possible = in.substring(in.indexOf(':') + 1).toCharArray();
+            String out = "";
+            for (int i = 0; i < possible.length; i++) {
+                out = out + possible[i];
+            }
+            currFreq.put(header, out);
+
+        }
+        freqScan.close();
+        // LOAD FREQUENCY TABLE
         File file = new File("C:\\Code\\CSC360\\Cryptanalyst\\dictionary.txt");
         Scanner scanner = new Scanner(file);
         File out = new File("C:\\Code\\CSC360\\Cryptanalyst\\outputs\\CUSTOM.txt");
         FileWriter writer = new FileWriter(out);
         ArrayList<Character> temp = new ArrayList<>();
-        String TESTONE = "BIMU"; // Z
-        String TESTTWO = "ADEINORSTV"; // W
-        String TESTTHREE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // H
-        String TESTFOUR = "ELNT"; // M
-        String TESTFIVE = "ABCDEGILMNOPRSTUVWXYZ"; // V
-        String TESTSIX = "CFIZ"; // Y
-        String TESTSEVEN = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // O
-        String TESTEIGHT = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // M
-        String TESTNINE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // F
+        String chars = "SW";
+        int[] occur = { 1, 1 };
+        if (occur.length != chars.length()) {
+            throw new Exception();
+        }
 
         while (scanner.hasNextLine()) {
             String in = scanner.nextLine();
             if (temp.size() == 26) {
                 break;
             }
-            if (in.length() == 7 && TESTONE.indexOf(in.toUpperCase().charAt(0)) != -1
-                    && TESTTWO.indexOf(in.toUpperCase().charAt(1)) != -1
-                    && TESTTHREE.indexOf(in.toUpperCase().charAt(2)) != -1
-                    && TESTFOUR.indexOf(in.toUpperCase().charAt(0)) != -1
-                    && TESTFIVE.indexOf(in.toUpperCase().charAt(1)) != -1
-                    && TESTSIX.indexOf(in.toUpperCase().charAt(5)) != -1
-                    && TESTSEVEN.indexOf(in.toUpperCase().charAt(6)) != -1/*
-                    && TESTEIGHT.indexOf(in.toUpperCase().charAt(7)) != -1
-                    && TESTNINE.indexOf(in.toUpperCase().charAt(8)) != -1
-                    && in.charAt(0) == in.charAt(6)*/) {
-                if (in.chars().filter(c -> c == in.charAt(0)).count() > 1
-                        || in.chars().filter(c -> c == in.charAt(1)).count() > 1
-                        || in.chars().filter(c -> c == in.charAt(2)).count() > 1
-                        || in.chars().filter(c -> c == in.charAt(3)).count() > 1
-                        || in.chars().filter(c -> c == in.charAt(4)).count() > 1
-                        || in.chars().filter(c -> c == in.charAt(5)).count() > 1
-                        || in.chars().filter(c -> c == in.charAt(6)).count() > 1/*
-                        || in.chars().filter(c -> c == in.charAt(7)).count() > 1
-                        || in.chars().filter(c -> c == in.charAt(8)).count() > 1*/) {
+            // Only check the word if it's the same length as the word we're using as a mask
+            // (chars).
+            if (in.length() == chars.length()) {
+                boolean skip = false;
+                for (int i = 0; i < in.length() && !skip; i++) {
+                    if (currFreq.get(chars.charAt(i)).indexOf(in.toUpperCase().charAt(i)) == -1) {
+                        skip = true; // For every character in our mask, make sure the corresponding character in
+                                     // this word is in our frequency list.
+                    }
+
+                    final int _i = i;
+
+                    if (in.chars().filter(c -> c == in.charAt(_i)).count() > occur[i]) {
+                        skip = true;
+                    }
+                }
+                if (skip) {
                     continue;
                 }
-                int i = 2;
+                int i = 1;
                 if (!temp.contains(in.charAt(i))) {
                     temp.add(in.charAt(i));
                     System.out.println(in);
